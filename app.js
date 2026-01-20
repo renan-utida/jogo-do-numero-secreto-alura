4dfaa9f131288c90775e22bb4dac9b20fffa69cd
Renan <renandiutida@gmail.com>
2026-01-20 16:37:51 -0300

    Adicionando algumas funçoes em nosso projeto

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+let numeroSecreto = gerarNumeroAleatorio();
 
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
