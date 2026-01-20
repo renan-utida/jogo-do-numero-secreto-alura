3a97e87b51ffa223083b7e1619687f40a6a4819f
Renan <renandiutida@gmail.com>
2026-01-20 01:21:03 -0300

    Manipulando textos em HTML usando JavaScript

--- app.js
+++ app.js
@@ -1,28 +1,6 @@
 
-alert('Boas vindas ao jogo do número secreto!');
-let numeroMaximo = 5000;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
-console.log(numeroSecreto);
-let chute;
-let tentativas = 1;
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
 
-// Enquanto não for igual ao N.S.
-while (chute != numeroSecreto) {
-
-    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
-
-    // Se chute for igual ao numero secreto
-    if (chute == numeroSecreto) {
-        break;
-    } else {
-        if (chute < numeroSecreto) {
-            alert(`Você errou! O número secreto é maior que ${chute}`);   
-        } else{
-            alert(`Você errou! O número secreto é menor que ${chute}`);
-        }
-        tentativas++;
-    }
-}
-
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-alert(`Isso aí! Você descobriu o número secreto é ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
