167c3197df0e4355c9d87208ea66ef052e5ee6c5
Renan <renandiutida@gmail.com>
2026-01-21 01:53:01 -0300

    Corrigindo bug

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ function exibirTextoNaTela(tag, texto){
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
 }
 
 exibirMensagemInicial();
